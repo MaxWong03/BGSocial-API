53ec69973bbc6bb82cc8e05a550ea7b55c462e2d
CamilaRivera <camila.riverart@gmail.com>
2019-10-25 16:58:40 +0000

    Added routes plays CRUD and helper functions for play routes

--- src/db/plays.js
+++ src/db/plays.js
@@ -1,111 +1,136 @@
-/**
- * Get a single user from the database given their id.
- * @param {string} id The id of the user.
- * @return {Promise<{}>} A promise to the user.
- */
+const playsColumnsNames = [
+  'date',
+  'duration',
+  'game_id',
+  'event_id'
+];
 
-// const todosColumnsNames = [
-//   'title',
-//   'description',
-//   'start_date',
-//   'end_date',
-//   'priority',
-//   'complete',
-//   'cover_photo_url',
-//   'todo_id',
-//   'category_id',
-// ];
+const playsUsersColumnsNames = [
+  'score',
+  'is_winner',
+  'user_id',
+  'play_id'
+];
 
-// const getUserById = function (db, id) {
-//   return db
-//     .query(`SELECT * FROM users WHERE id = $1;`, [id])
-//     .then(res => res.rows[0]);
-// };
-
-const getPlaysByUserId = function (db, userId) {
-  return db.query(`SELECT plays_users.* FROM plays_users JOIN plays ON plays_users.play_id = plays.id WHERE plays_users.user_id = $1`, [userId])
+const getGamesByIds = function (db, gameIds) {
+  return db.query(`SELECT * FROM games WHERE id = ANY($1::int[])`, [gameIds])
     .then(res => res.rows);
 };
 
-// const addCategory = function (db, category) {
-//   return db.query(`INSERT INTO categories (description, user_id, cover_photo_url, main_category)
-//     VALUES ($1, $2, $3, $4) RETURNING *;`, [category.description, category.user_id, category.cover_photo_url, category.main_category])
-//     .then(res => res.rows[0]);
-// };
+const getPlaysByUserId = function (db, userId) {
+  return db.query(`SELECT plays.*
+   FROM plays_users JOIN plays ON plays_users.play_id = plays.id
+   WHERE plays_users.user_id = $1`, [userId])
+    .then(res => res.rows);
+};
 
-// const deleteCategory = function (db, id, userId) {
-//   return db.query(`DELETE FROM categories WHERE id = $1 AND user_id = $2`, [id, userId]);
-// };
+const isUserInPlay = function (db, playId, userId) {
+  return db.query(`SELECT plays_users.id
+   FROM plays_users
+   WHERE plays.user_id = $1 AND plays.play_id = $2`, [userId, playId])
+    .then(res => res.rows.length > 0);
+};
 
-// const updateCategory = function (db, editableCategory) {
-//   return db.query(`UPDATE categories SET description = $1, cover_photo_url = $2, main_category = $3 WHERE user_id = $4 AND id = $5 RETURNING *;`
-//     , [editableCategory.description, editableCategory.cover_photo_url, editableCategory.main_category, editableCategory.user_id, editableCategory.id])
-//     .then(res => res.rows[0]);
-// };
-
-// const getTodosByCategoryId = function (db, userId, categoryID) {
-//   return db.query(`SELECT * FROM todos JOIN categories ON todos.category_id = categories.id WHERE
-//   categories.user_id = $1 AND categories.id = $2`, [userId, categoryID])
+// const getPlaysUserByUserId = function (db, userId) {
+//   return db.query(`SELECT plays_users.*
+//    FROM plays_users JOIN plays ON plays_users.play_id = plays.id
+//    WHERE plays_users.user_id = $1`, [userId])
 //     .then(res => res.rows);
 // };
 
-// const getTodosByUserId = function (db, userId) {
-//   return db.query(`SELECT todos.* FROM todos JOIN categories ON todos.category_id = categories.id WHERE
-//   categories.user_id = $1`, [userId])
-//     .then(res => {
-//       return res.rows});
-// };
+const getPlaysUserByPlayIds = function (db, playIds) {
+  return db.query(`SELECT plays_users.*
+   FROM plays_users JOIN plays ON plays_users.play_id = plays.id
+   WHERE plays.id = ANY($1::int[])`, [playIds])
+    .then(res => res.rows);
+};
 
-// const getTodoById = function (db, userId, todoId) {
-//   return db.query(`SELECT todos.* FROM todos JOIN categories ON todos.category_id = categories.id WHERE
-//   categories.user_id = $1 AND todos.id = $2`, [userId, todoId])
-//     .then(res => res.rows[0]);
-// };
+const getPlaysByGameId = function (db, gameId) {
+  return db.query(`SELECT * FROM plays WHERE plays.game_id = $1`, [gameId])
+    .then(res => res.rows);
+};
 
-// const updateTodo = function (db, todo, userId) {
-//   const validColumns = todosColumnsNames.filter(column => column in todo);
-//   const values = validColumns.map(column => todo[column]);
+//get top score by user and by game?
 
-//   const sets = values.map((value, index) => `${validColumns[index]} = $${index + 1}`)
+const addPlay = function (db, play) {
+  const validColumns = playsColumnsNames.filter(column => column in play);
+  const indexArray = validColumns.map((column, index) => `$${index + 1}`);
+  const values = validColumns.map(column => play[column]);
 
-//   values.push(todo.id);
-//   values.push(userId);
+  return db.query(`INSERT INTO plays (${validColumns.join(', ')})
+    VALUES (${indexArray.join(', ')}) RETURNING *;`, values)
+    .then(function (res) {
+      return res.rows[0];
+    });
+};
 
-//   let query = `UPDATE todos
-//     SET ${sets.join(', ')}
-//     WHERE id = $${values.length - 1} AND todos.category_id = ANY(
-//       SELECT id FROM categories WHERE categories.user_id = $${values.length})
-//     RETURNING *;`
+const addUserPlay = function (db, userPlay) {
+  const validColumns = playsUsersColumnsNames.filter(column => column in userPlay);
+  const indexArray = validColumns.map((column, index) => `$${index + 1}`);
+  const values = validColumns.map(column => userPlay[column]);
 
-//   return db.query(query, values)
-//     .then(res => res.rows[0]);
-// };
+  return db.query(`INSERT INTO plays_users (${validColumns.join(', ')})
+    VALUES (${indexArray.join(', ')}) RETURNING *;`, values)
+    .then(function (res) {
+      return res.rows[0];
+    });
+};
 
-// const addTodo = function (db, todo) {
-//   const validColumns = todosColumnsNames.filter(column => column in todo);
-//   const indexArray = validColumns.map((column, index) => `$${index + 1}`)
-//   const values = validColumns.map(column => todo[column]);
+const deleteUserPlay = function (db, id, userId) {
+  return db.query(`DELETE FROM plays_users WHERE plays_users.play_id = $1 AND plays_users.user_id = $2`, [id, userId]);
+};
 
-//   return db.query(`INSERT INTO todos (${validColumns.join(', ')})
-//     VALUES (${indexArray.join(', ')}) RETURNING *;`, values)
-//     .then(function(res) {
-//       return res.rows[0];
-//     });
-// };
+const updateUserPlay = function (db, userPlay) {
+  const validColumns = playsUsersColumnsNames.filter(column => column in userPlay);
+  const values = validColumns.map(column => userPlay[column]);
 
-// const deleteTodo = function (db, id, userId) {
-//   return db.query(`DELETE FROM todos WHERE todos.id = $1 AND todos.category_id = ANY(
-//     SELECT id FROM categories WHERE user_id = $2)`, [id, userId]);
-// };
+  const sets = values.map((value, index) => `${validColumns[index]} = $${index + 1}`)
 
-// module.exports = {
-//   addCategory, getUserById,
-//   getPlaysByUserId, deleteCategory, updateCategory, getTodosByCategoryId,
-//   getTodosByUserId, getTodoById, addTodo, updateTodo, deleteTodo
-// };
+  values.push(userPlay.user_id);
+  values.push(userPlay.play_id);
+
+  const query = `UPDATE plays_users
+    SET ${sets.join(', ')}
+    WHERE plays_users.user_id = $${values.length - 1} AND plays_users.play_id = $${values.length}
+    RETURNING *;`
+
+  return db.query(query, values)
+    .then(res => res.rows[0]);
+};
+
+const updatePlay = function (db, play) {
+  const validColumns = playsColumnsNames.filter(column => column in play);
+  const values = validColumns.map(column => play[column]);
+
+  const sets = values.map((value, index) => `${validColumns[index]} = $${index + 1}`)
+
+  values.push(play.id);
+
+  let query = `UPDATE plays
+    SET ${sets.join(', ')}
+    WHERE plays.id = $${values.length}
+    RETURNING *;`
+
+  return db.query(query, values)
+    .then(res => res.rows[0]);
+};
+
+const deletePlay = function (db, id) {
+  return db.query(`DELETE FROM plays WHERE plays.id = $1`, [id]);
+};
+
 
 module.exports = {
-  getPlaysByUserId
+  isUserInPlay,
+  getGamesByIds,
+  getPlaysByUserId,
+  getPlaysUserByPlayIds,
+  addPlay,
+  addUserPlay,
+  deleteUserPlay,
+  updateUserPlay,
+  updatePlay,
+  deletePlay
 };
 
 
--- src/routes/plays.js
+++ src/routes/plays.js
@@ -1,15 +1,128 @@
 const router = require("express").Router();
 
 const { getLoggedUserId } = require('../utils');
-const { getPlaysByUserId } = require('../db/plays.js');
+const {
+  isUserInPlay,
+  getPlaysByUserId,
+  getPlaysUserByPlayIds,
+  getGamesByIds,
+  addPlay,
+  addUserPlay,
+  deleteUserPlay,
+  updateUserPlay,
+  updatePlay,
+  deletePlay
+} = require('../db/plays.js');
+
+// Avoiding too many requests, we are returning all related data
+// /plays
+// /games/1
+// /games/2
+// /plays/1/plays_users
+// /plays/4/plays_users
+
+// TODO: Add parameter to make it optional to return the related data
+
+// /games/4/statistics?userId
+
+const errorHandler = (res) => {
+  return err => {
+    console.log(err);
+    res
+      .status(500)
+      .json({ error: err.message });
+  }
+};
 
 module.exports = db => {
-  router.get("/plays", (req, res) => {
+  router.get("/", (req, res) => {
     const userId = getLoggedUserId(req);
     getPlaysByUserId(db, userId)
-      .then(data => {
-        res.json({ plays: data });
+      .then(plays => {
+        const gameIds = plays.map(play => play.game_id);
+        const playsIds = plays.map(play => play.id);
+
+        Promise.all([
+          getPlaysUserByPlayIds(db, playsIds),
+          getGamesByIds(db, gameIds)
+        ]).then(([playsUsers, games]) => {
+          const gamesById = games.reduce((accum, item) => {
+            accum[item.id] = item;
+            return accum;
+          }, {});
+
+          plays.forEach(play => {
+            play.playsUsers = playsUsers.filter(playUser => playUser.play_id == play.id);
+          });
+
+          res.json({ plays: plays, games: gamesById });
+        }).catch(e => console.log(e));
+
+      });
+  });
+
+  router.post("/", (req, res) => {
+    addPlay(db, req.body)
+      .then(play => {
+        const promises = req.body.playsUsers.map(playsUser => addUserPlay(db, { ...playsUser, play_id: play.id }));
+        Promise.all(promises)
+          .then(playsUsers => {
+            res.json({ ...play, playsUsers });
+          });
       })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+  router.post("/:id/delete", (req, res) => {
+    const userId = getLoggedUserId(req);
+    deleteUserPlay(db, req.params.id, userId)
+      .then(() => {
+        res.send("Success");
+        getPlaysUserByPlayIds(db, [req.params.id])
+          .then((playsUsers) => {
+            if (playsUsers.length === 0) {
+              deletePlay(db, req.params.id)
+            };
+          });
+      })
+      .catch(err => {
+        console.log("About to error out", err);
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+  router.post("/:id/edit", (req, res) => {
+    const userId = getLoggedUserId(req);
+
+    console.log('req.params.id', typeof req.params.id);
+    console.log('req.body.id', typeof req.body.id);
+    console.log('userId', userId);
+    if (!isUserInPlay(db, req.params.id, userId) || req.params.id !== `${req.body.id}`) {
+      res
+        .status(403)
+        .json({ error: "Forbidden" });
+      return;
+    }
+
+    updatePlay(db, req.body)
+      .then(play => {
+        const promises = req.body.playsUsers.map(playByUser => updateUserPlay(db, playByUser));
+        Promise.all(promises)
+          .then(playsUsers => {
+            res.json({ ...play, playsUsers });
+          });
+      }).catch(err => {
+        console.log(err);
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
   });
 
   return router;
