caecc5ba1cfd0e6bc20951653f374fa37931ce03
zongxili <zongxiliwork@gmail.com>
2019-10-26 16:23:19 -0700

    changed some routes and added 'added user game'

--- src/db/selectors/games.js
+++ src/db/selectors/games.js
@@ -5,6 +5,11 @@ const getAllGamesFromDB = function (db) {
     .then(res => res.rows);
 };
 
+const addUserGamesgetOnePublicGameByGameID = function (db, gameID) {
+  // return db.query(`SELECT * FROM games WHERE games.id = $1`, [gameID])
+  //   .then(res => res.rows);
+};
+
 const getOnePublicGameByGameID = function (db, gameID) {
   return db.query(`SELECT * FROM games WHERE games.id = $1`, [gameID])
     .then(res => res.rows);
@@ -20,30 +25,41 @@ const getOnePublicGameByPattern = function (db, searchPattern) {
 // get a list of games by searching the categories name using ILIKE
 // just save it for now, since the app wont be able to search the games by uncompleted/ case insensitived categories names
 const getAllGameIDsByCategorySearchingPattern = function (db, categorySearchingPattern) {
-  return db.query(`select category from games where category ILIKE '%${categorySearchingPattern}%'`)
+  return db.query(`select * from games where category ILIKE '%${categorySearchingPattern}%'`)
     .then(res => res.rows);
 };
 
 // get a list of games owned by one user based on the User id
 const getAllGamesByUserID = function (db, userID) {
-  return db.query(`SELECT * FROM user_games WHERE user_id = ${userID}`)
+  return db.query(`SELECT games.* FROM games JOIN user_games on games.id = user_games.game_id WHERE user_games.user_id = ${userID}`)
     .then(res => res.rows);
 };
 
-// get all the games for one specific event
-const getAllGamesByEventID = function (db, eventID) {
-  return db.query(`select * from event_games where event_id = ${eventID}`)
+// get a game owned by one user based on the User id and game ID
+const getOneGameByUserID = function (db, userID, gameID) {
+  return db.query(`SELECT games.* FROM games JOIN user_games on games.id = user_games.game_id WHERE user_games.user_id = ${userID} AND user_games.game_id = ${gameID}`)
     .then(res => res.rows);
+};
+
+// get all the games for one specific even
+const getAllGamesByEventID = function (db, eventID) {
+  return db.query(`  select games.* from event_games JOIN games ON games.id = event_games.game_id where event_id = ${eventID}`)
+    .then(res => res.rows);
+
 };
 
 // find the game a user play in a specific event based on the user id and event id
 // conditions: user must join the event (attendances.is_confirmed = TRUE)
-// this is commented out since it is too similar with the getAllGamesByEventID
 // since we assume all players in the event will play every game in the event
 //
-// const getAllGamesByUserIDNEventID = function (db, userID, eventID) {
-//   return db.query(`SELECT * FROM event_games JOIN attendances ON attendances.event_id = event_games.event_id WHERE event_games.event_id = ${eventID} AND attendances.is_confirmed = TRUE AND attendances.attendant_id = ${userID};`)
-//     .then(res => res.rows);
-// };
+const getAllGamesForPlayerInEvent = function (db, userID, eventID) {
+  return db.query(`SELECT * FROM event_games JOIN attendances ON attendances.event_id = event_games.event_id WHERE event_games.event_id = ${eventID} AND attendances.is_confirmed = TRUE AND attendances.attendant_id = ${userID};`)
+    .then(res => res.rows);
+};
 
-module.exports = { getAllGamesFromDB, getOnePublicGameByGameID, getOnePublicGameByPattern, getAllGameIDsByCategorySearchingPattern, getAllGamesByUserID, getAllGamesByEventID };
+const addUserGame = function (db, gameID, userID ) {
+  return db.query(`INSERT INTO user_games (game_id, user_id) VALUES (${gameID}, ${userID});`)
+    .then(res => res);
+};
+
+module.exports = { getAllGamesFromDB, getOnePublicGameByGameID, getOnePublicGameByPattern, getAllGameIDsByCategorySearchingPattern, getAllGamesByUserID, getOneGameByUserID, getAllGamesForPlayerInEvent, addUserGame };
--- src/routes/games.js
+++ src/routes/games.js
@@ -1,11 +1,16 @@
 const router = require("express").Router();
 const { getLoggedUserId } = require('../utils'); // get the user ID based on the login user
-const { getAllGamesFromDB, getOnePublicGameByGameID, getOnePublicGameByPattern, getAllGameIDsByCategorySearchingPattern, getAllGamesByUserID, getAllGamesByEventID, getAllGamesByUserIDNEventID }= require('../db/selectors/games');
+const { getAllGamesFromDB, getOnePublicGameByGameID, getOnePublicGameByPattern, getAllGameIDsByCategorySearchingPattern, getAllGamesByUserID, getOneGameByUserID, getAllGamesByEventID, getAllGamesForPlayerInEvent, addUserGame } = require('../db/selectors/games');
+
+const user_gamesColumnsNames = [
+  'game_id',
+  'user_id'
+];
 
 module.exports = db => {
   // get all the PUBLIC games from database
-  // this si refered as all game library
-  router.get("/library/games", (req, res) => {
+  // this is refered as all game library
+  router.get("games/library", (req, res) => {
     // const userId = getLoggedUserId(req);
     getAllGamesFromDB(db)
       .then(data => {
@@ -14,42 +19,52 @@ module.exports = db => {
   });
 
   // get one game from public library
-  router.get("/games/:gameID", (req, res) => {
+  router.get("/games/library/:gameID", (req, res) => {
     const gameID = req.params.gameID;
     getOnePublicGameByGameID(db, gameID)
       .then(data => {
-        res.json( {game: data} );
+        res.json( { selectedGame: data } );
       })
   });
 
   // find games matching the entered pattern in game library
-  router.get("/games/searchNames/:gamePattern", (req, res) => {
+  router.get("/games/library/searchNames/:gamePattern", (req, res) => {
     const gamePattern = req.params.gamePattern;
     console.log(gamePattern);
     getOnePublicGameByPattern(db, gamePattern)
       .then(data => {
-        res.json({ gamesMatchingPattern: data });
+        res.json({ gamesMatchingNamePattern: data });
       })
   });
 
   // find games matching the entered Category pattern in game library
-  router.get("/games/searchCategories/:categorySearchingPattern", (req, res) => {
+  router.get("/games/library/searchCategories/:categorySearchingPattern", (req, res) => {
     const categorySearchingPattern = req.params.categorySearchingPattern;
     getAllGameIDsByCategorySearchingPattern(db, categorySearchingPattern)
       .then(data => {
-        res.json({ games: data });
+        res.json({ gamesMatchingCategoryPattern: data });
       })
   });
 
   // get all the games owned by a user by given user ID
-  router.get("/games", (req, res) => {
-    const userId = getLoggedUserId(req);
+  router.get("/user/games/:userID", (req, res) => {
+    const userId = req.params.userID;
     getAllGamesByUserID(db, userId)
       .then(data => {
         res.json( {gamesByUserID: data} );
       })
   });
 
+  // get all the games owned by a user by given user ID
+  router.get("/user/games/:userID/:gameID", (req, res) => {
+    const userID = req.params.userID;
+    const gameID = req.params.gameID;
+    getOneGameByUserID(db, userID, gameID)
+      .then(data => {
+        res.json( {aGameByUserIDGameID: data} );
+      })
+  });
+  
   // getAllGamesByEventID
   router.get("/event/:eventID/games", (req, res) => {
     const eventID = req.params.eventID;
@@ -59,15 +74,22 @@ module.exports = db => {
       })
   });
 
-  // getAllGamesByUserIDNEventID
-  // router.get("/event/:eventID/games/testing", (req, res) => {
-  //   const eventID = req.params.eventID;
-  //   const userId = getLoggedUserId(req);
-  //   getAllGamesByEventID(db, userId, eventID)
-  //     .then(data => {
-  //       res.json( {getAllGamesByEventID: data} );
-  //     })
-  // });
+  router.get("/event/:eventID/games/testing", (req, res) => {
+    const eventID = req.params.eventID;
+    const userId = getLoggedUserId(req);
+    getAllGamesByEventID(db, userId, eventID)
+      .then(data => {
+        res.json( {getAllGamesByEventID: data} );
+      })
+  });
 
+  router.post("/user/add/games/:userID/:gameID", (req, res) => {
+    const userID = req.params.userID;
+    const gameID = req.params.gameID;
+    addUserGame(db, gameID, userID)
+      .then(data => {
+        res.json( { theAddedGame: data} );
+      })
+    });
   return router;
 };
