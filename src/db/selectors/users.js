08f02c861da5ea8ad8d948c61e5c4fd10125b74a
MaxWong03 <madmaxmmajournal@gmail.com>
2019-10-27 23:03:18 +0000

    modfiy seed string, change query to get back friend related info

--- src/db/selectors/users.js
+++ src/db/selectors/users.js
@@ -11,7 +11,7 @@ const createUser = (db) => {
   VALUES ('Max Wong', 'Mad Max', 'maxwong93@gmail.com', '123456', 'https://cdn.vox-cdn.com/thumbor/IKt535q8LMnJDddmLL74TBtzv88=/0x266:1024x949/1280x854/cdn.vox-cdn.com/uploads/chorus_image/image/48942277/N3DS_PokemonSuperMysteryDungeon_MainIllustration_png_jpgcopy.0.0.jpg')
   `)
     .then(() => console.log('Successfully Creating User'))
-    .catch(error => console.log("Error Creating User:" ,error));
+    .catch(error => console.log("Error Creating User:", error));
 };
 
 
@@ -23,16 +23,17 @@ const getUserByFBId = (db, fbId) => {
     .catch(error => console.log('getUserByFBId Error:', error));
 };
 
-const getFriendsIdByUserId = (db, userId)  => {
+const getFriendsIdByUserId = (db, userId) => {
+  //friend name, friend avatar
   return db.query(`
-    SELECT * 
-    FROM friends 
-    WHERE (user1_id = $1 OR user2_id = $2) AND is_accepted = TRUE
-`, [userId, userId])
-  .then(res => {
-    const friendsIds = res.rows.map(row => row.user1_id !== userId ? row.user1_id : row.user2_id);
-    return friendsIds;
-  });
+  Select user2_id AS friend_id, users.name AS friend_name, users.avatar AS friend_avatar
+  FROM friends JOIN users 
+  on user2_id = users.id
+  where user1_id = $1 
+  AND is_accepted IS TRUE
+`, [userId])
+    .then(res => res.rows)
+    .catch(err => console.log('getFriendsIdByUserId Error:', err))
 };
 
 module.exports = {
