40f8023893a5c8b0233219d744c6e1d20d32074e
MaxWong03 <madmaxmmajournal@gmail.com>
2019-10-27 00:37:57 +0000

    modify seed data for friends association

--- fake-user-seed/makeFriends.js
+++ fake-user-seed/makeFriends.js
@@ -7,13 +7,16 @@ for (let i = 1; i <= 29; i++) {
   sqlQuery += `(TRUE, 1, ${i+1}),\n`
 }
 
+sqlQuery += `(TRUE, 2, 1), \n`;
 for (let i = 2; i <= 29; i++) {
   sqlQuery += `(TRUE, 2, ${i+1}),\n`
 }
 
+sqlQuery += `(TRUE, 3, 1), \n`
+sqlQuery += `(TRUE, 3, 2), \n`
 for (let i = 3; i <= 29; i++) {
   sqlQuery += `(TRUE, 3, ${i+1})`
   i === 29 ? sqlQuery += ';\n' : sqlQuery += ',\n';
 }
 
-fs.writeFileSync('../src/db/seeds/test.sql', sqlQuery);
+fs.writeFileSync('../src/db/seeds/13_friends.sql', sqlQuery);
