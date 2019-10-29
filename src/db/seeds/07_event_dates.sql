0ac28e588f1501004f38000a7059d06da4b9fd38
Camila Rivera <camila.riverart@gmail.com>
2019-10-28 17:20:33 -0700

    Added event_dates to api events

--- src/db/seeds/07_event_dates.sql
+++ src/db/seeds/07_event_dates.sql
@@ -1,7 +1,9 @@
 INSERT INTO event_dates (id, date, location, event_id, is_chosen)
 VALUES (1, '2019-12-03 13:23:44', '401 W Georgia St #600, Vancouver, BC V6B 5A1', 1, TRUE),
 (2, '2019-12-11 17:23:44', '999 Canada Pl, Vancouver, BC V6C 3T4', 2, FALSE),
-(3, '2019-11-30 22:30:44', '305 Water St, Vancouver, BC V6B 1B9', 3, TRUE),
-(4, '2019-11-28 13:00:44', '401 W Georgia St #600, Vancouver, BC V6B 5A1', 1, FALSE);
+(3, '2019-12-13 17:23:44', '888 Canada Pl, Vancouver, BC V6C 3T4', 2, FALSE),
+(4, '2019-12-14 17:23:44', '777 Canada Pl, Vancouver, BC V6C 3T4', 2, FALSE),
+(5, '2019-11-30 22:30:44', '305 Water St, Vancouver, BC V6B 1B9', 3, TRUE),
+(6, '2019-11-28 13:00:44', '401 W Georgia St #600, Vancouver, BC V6B 5A1', 1, FALSE);
 
 SELECT SETVAL('event_dates_id_seq', 50);
