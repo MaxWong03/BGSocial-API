a9c77e944c58b4d5b9b321ff2eab24d7e97542bf
MaxWong03 <madmaxmmajournal@gmail.com>
2019-11-01 19:51:09 +0000

    remove unncessary route

--- src/application.js
+++ src/application.js
@@ -44,7 +44,6 @@ module.exports = function application(
   app.use(bodyparser.json());
 
   // app.use("/api", appointments(db, actions.updateAppointment));
-  app.use("/api/", gamesRouter(db));
   app.use("/api/users", usersRouter(db));
   app.use("/api/plays", playsRouter(db));
   app.use("/api/events", eventsRouter(db));
