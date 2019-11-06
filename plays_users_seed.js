8099011d11c096fd953f20f0ab4307e1c10825e3
MaxWong03 <madmaxmmajournal@gmail.com>
2019-11-06 02:52:28 +0000

    write seeds for 11 and 12

--- plays_users_seed.js
+++ plays_users_seed.js
@@ -3,49 +3,52 @@ const fs = require('fs');
 const getRandomScore = () => {
   min = Math.ceil(0);
   max = Math.floor(100);
-  return Math.floor(Math.random() * (100 - 0 + 1)) + 0; 
+  return Math.floor(Math.random() * (100 - 0 + 1)) + 0;
 }
 
 const getRandomUser = () => {
   min = Math.ceil(1);
   max = Math.floor(30);
-  return Math.floor(Math.random() * (30 - 1 + 1)) + 1; 
+  return Math.floor(Math.random() * (30 - 1 + 1)) + 1;
 }
-const scoreArray = [getRandomScore(), getRandomScore(), getRandomScore(), getRandomScore(), getRandomScore()];
 
-console.log(getRandomUser());
+const getScoreArr = () => {
+  return [getRandomScore(), getRandomScore(), getRandomScore(), getRandomScore(), getRandomScore()];
+}
 
+const getUniqueUserArr = () => {
+  const userArr = [];
+  while (userArr.length !== 5) {
+    const randomUser = getRandomUser();
+    if (!userArr.includes(randomUser)) userArr.push(randomUser);
+  }
+  return userArr;
+}
 
+const getWinningScore = (scoreArr) => {
+  return Math.max(...scoreArr);
+}
 
-// let sqlQuery = `INSERT INTO plays_users (score, is_winner, user_id, play_id)
-// VALUES
-// (34, FALSE, 1, 1),
-// (2, FALSE, 1, 2),
-// (34, TRUE, 3, 3),
-// (10, FALSE, 1, 3),
-// (10, FALSE, 4, 7),
-// (45, FALSE, 5, 1),
-// (60, TRUE, 6, 1),
-// (66, FALSE, 7, 2),
-// (66, FALSE, 8, 2),
-// (67, TRUE, 9, 2),
-// (25, FALSE, 1, 4),
-// (30, FALSE, 2, 4),
-// (50, TRUE, 3, 4),
-// (65, FALSE, 1, 5),
-// (67, TRUE, 2, 5),
-// (60, FALSE, 4, 5),
-// (80, FALSE, 3, 6),
-// (81, FALSE, 5, 6),
-// (82, TRUE, 1, 6),
-// (56, FALSE, 1, 7),
-// (57, TRUE, 6, 7),
-// (56, FALSE, 7, 7),
-// (66, FALSE, 10, 10),
-// (67, FALSE, 10, 10),
-// (76, TRUE, 10, 10);
-// `;
+let sqlQuery = `INSERT INTO plays_users (score, is_winner, user_id, play_id)
+VALUES
+`;
 
+for (let i = 1; i <= 100; i++) {
+  const userArr = getUniqueUserArr();
+  const scoreArr = getScoreArr();
+  for (let j = 0; j < 5; j++) {
+    const winningScore = getWinningScore(scoreArr);
+    const score = scoreArr[j];
+    const user = userArr[j];
+    const isWinner = score === winningScore ? true : false;
+    i === 100 && j === 4 ?
+    sqlQuery += `(${score}, ${isWinner}, ${user}, ${i});\n`
+    :
+    sqlQuery += `(${score}, ${isWinner}, ${user}, ${i}),\n`
+  }
 
+} 
 
-// fs.writeFileSync('./src/db/seeds/fake-plays-users.sql', sqlQuery);
+sqlQuery += "SELECT SETVAL('plays_users_id_seq', 550);"
+
+fs.writeFileSync('./src/db/seeds/12_plays_users.sql', sqlQuery);
