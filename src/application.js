c3b077d5bfb66d4226ded36c3081f78a1718aafc
Camila Rivera <camila.riverart@gmail.com>
2019-11-07 13:43:51 -0800

    Fixed small bug in statsitics

--- src/application.js
+++ src/application.js
@@ -74,29 +74,7 @@ module.exports = function application(
         }
       });
   });
-
-
-  // Maybe we will use it later for test 
-  // if (ENV === "development" || ENV === "test") {
-  //   Promise.all([
-  //     read(path.resolve(__dirname, `db/schema/create.sql`)),
-  //     read(path.resolve(__dirname, `db/schema/${ENV}.sql`))
-  //   ])
-  //     .then(([create, seed]) => {
-  //       app.get("/api/debug/reset", (request, response) => {
-  //         db.query(create)
-  //           .then(() => db.query(seed))
-  //           .then(() => {
-  //             console.log("Database Reset");
-  //             response.status(200).send("Database Reset");
-  //           });
-  //       });
-  //     })
-  //     .catch(error => {
-  //       console.log(`Error setting up the reset route: ${error}`);
-  //     });
-  // }
-
+  
   app.close = function() {
     return db.end();
   };
